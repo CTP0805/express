e8d7d068ca3be5efd3b1291a9e165190229b5c5c
Kenny94crazy <kenny94crazy@gmail.com>
2026-07-07 16:02:20 +0800

    第一次變更

--- routes/api-auth.ts
+++ routes/api-auth.ts
@@ -1,37 +1,38 @@
 import express, { type Request, type Response, Router } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import pool from "../utils/connect-mysql.js";
 
 const router: Router = Router();
 
 // JWT 登入
 router.post("/login", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+  const { account, password } = req.body;
   // 1. 先判斷兩個欄位是不是都有值
-  if (!email || !password) {
-    res.json({ success: false, message: "帳號或密碼錯誤", code: 400 });
+  if (!account || !password) {
+    res.status(400).json({ success: false, message: "請填寫帳號或密碼(後端)"});
     return;
   }
   // 2. 帳號對不對
-  const member = await prisma.member.findUnique({
-    where: {
-      email,
-    },
-  });
+  const sql = `SELECT * FROM member WHERE email = ?`
+  const [member] = await pool.query(sql, [account]);
+  console.log(member);
+  
   if (!member) {
-    res.json({ success: false, message: "帳號或密碼錯誤", code: 402 });
+    res.status(402).json({ success: false, message: "帳號或密碼錯誤(後端)"});
     return;
   }
+  
   // 3. 密碼對不對
-  const result = await bcrypt.compare(password, member.password_hash);
+  const result = await bcrypt.compare(password, member[0].password_hash);
   if (result) {
-    const { member_id, email, nickname } = member;
+    const { id, name, email } = member[0];
     // 回傳 JWT token (之前是寫入 session)
-    const payload = { member_id, email };
+    const payload = { id, email };
     const token = jwt.sign(payload, process.env.JWT_SECRET || "JWT_KEY");
-    res.json({ success: true, message: "登入成功", data: {id : member_id, email, nickname, token} });
+    res.status(200).json({ success: true, message: "登入成功(後端)", data: {id, name, email, token} });
   } else {
-    res.json({ success: false, message: "帳號或密碼錯誤", code: 406 });
+    res.status(406).json({ success: false, message: "帳號或密碼錯誤(後端)" });
   }
 });
 
